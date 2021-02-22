b178b176bfb93be83162bad8f1441e33ed4b37f9
Ananya Hans <ananyahans@Ananyas-iMac.centurylink.net>
2021-02-21 19:57:47 -0600

    02

--- problems/02-sentence-maker.js
+++ problems/02-sentence-maker.js
@@ -9,13 +9,23 @@ Examples:
 
 ***********************************************************************/
 
-sentenceMaker = (...strings) => {
-	return strings.join(" ") + "!"
+// sentenceMaker = (...strings) => {
+// 	return strings.join(" ") + "!"
+// }
+
+const sentenceMaker = (...strings)=> {
+ let finalStr = "" ;
+   for(let i = 0; i < strings.length; i ++){
+     let eachWord = strings[i];
+     finalStr +=  " " + eachWord 
+   }
+	let finalAns = (finalStr + "!").trim();
+	return finalAns
 }
 
-sentenceMaker('Hello', 'World'); // 'Hello World!'
-sentenceMaker('I', 'love', 'coding'); // 'I love coding!'
-sentenceMaker('The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog'); // 'The quick brown fox jumps over the lazy dog!'
+console.log(sentenceMaker('Hello', 'World')); // 'Hello World!'
+console.log(sentenceMaker('I', 'love', 'coding')); // 'I love coding!'
+console.log(sentenceMaker('The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog')); // 'The quick brown fox jumps over the lazy dog!'
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
 	module.exports = sentenceMaker;
